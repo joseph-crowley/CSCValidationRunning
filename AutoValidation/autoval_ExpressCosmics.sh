b4b7cf161661452d6896561a562485830dcfaca3
Devin Taylor <dntaylor@wisc.edu>
2015-03-19 15:41:23 +0100

    change autoval to absolute path

--- AutoValidation/autoval_ExpressCosmics.sh
+++ AutoValidation/autoval_ExpressCosmics.sh
@@ -3,6 +3,8 @@ aklog
 
 source /afs/cern.ch/cms/cmsset_default.sh
 
+cd /afs/cern.ch/cms/CAF/CMSCOMM/COMM_CSC/CSCVAL/AUTOVAL/CMSSW_7_3_5/src/CSCValidationRunning/AutoValidation
+
 eval `scramv1 runtime -sh`
 scram b
 
