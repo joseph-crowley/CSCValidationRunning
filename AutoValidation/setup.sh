8764aca7585278449828a8a5a18c7d3376ed8ec6
Devin Taylor <dntaylor@wisc.edu>
2015-03-19 12:50:04 +0100

    adding setup script

---
+++ AutoValidation/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+: ${CMSSW_BASE:?"CMSSW_BASE is not set! Run cmsenv before recipe.sh"}
+
+# Check CMSSW version
+MAJOR_VERSION=`echo $CMSSW_VERSION | sed "s|CMSSW_\([0-9]\)_.*|\1|"`
+MINOR_VERSION=`echo $CMSSW_VERSION | sed "s|CMSSW_\([0-9]\)_\([0-9]\)_.*|\2|"`
+
+echo "Detected CMSSW version: $MAJOR_VERSION $MINOR_VERSION"
+
+pushd $CMSSW_BASE/src
+
+ln -s RecoLocalMuon CSCValidationRunning/RecoLocalMuon
+
+scram b -j 8
+
+popd
+
+echo "Now setup new crontab (did you remember to kill your old one?)."
