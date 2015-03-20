abab207d1e8ce51b300bc68da2a92a27de98703f
Devin Taylor <dntaylor@wisc.edu>
2015-03-20 08:29:27 +0100

    fix to merge script

--- AutoValidation/run_cscval.py
+++ AutoValidation/run_cscval.py
@@ -239,8 +239,8 @@ def process_output(dataset,globalTag,**kwargs):
         run = runStr[3:]
 
         # some job still running. skip.
-        if "Job <"+run+"*> is not found" not in subprocess.Popen("unbuffer bjobs -J %s_%s*" % (run,stream), shell=True,stdout=pipe).communicate()[0].splitlines():
-            print 'Run %s not finished' % run
+        if "Job <%s_%s*> is not found" % (run,stream) not in subprocess.Popen("unbuffer bjobs -J %s_%s*" % (run,stream), shell=True,stdout=pipe).communicate()[0].splitlines():
+            print 'Run %s %s not finished' % (run,stream)
             continue
 
         rundir = 'run_%s' % run
