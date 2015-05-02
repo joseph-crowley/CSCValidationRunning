fac88c3caa27e49538c78bef2cd1ca78c505e57f
Devin Taylor <dntaylor@wisc.edu>
2015-05-02 22:01:03 +0200

    typo

--- AutoValidation/run_cscval.py
+++ AutoValidation/run_cscval.py
@@ -288,7 +288,7 @@ def process_output(dataset,globalTag,**kwargs):
         run = runStr[3:]
         if runN:
             if str(runN) != run: continue
-        if int(rn)<239636: continue # start of beam commissioning
+        if int(run)<239636: continue # start of beam commissioning
 
         # some job still running. skip.
         #if "Job <%s_%s*> is not found" % (run,stream) not in subprocess.Popen("unbuffer bjobs -J %s_%s*" % (run,stream), shell=True,stdout=pipe).communicate()[0].splitlines():
