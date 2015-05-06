b879733d6b98b0c3f58d1599795edbf003119bec
Devin Taylor <dntaylor@wisc.edu>
2015-05-06 16:17:08 +0200

    changing trigger method

--- AutoValidation/run_cscval.py
+++ AutoValidation/run_cscval.py
@@ -75,7 +75,7 @@ def python_mkdir(dir):
             pass
         else: raise
 
-def replace(map, fileInName, fileOutName):
+def replace(map, fileInName, fileOutName, moreLines=[]):
     '''Replace template file parameters'''
     replace_items = map.items()
     open(fileOutName,'w').close()
@@ -85,6 +85,11 @@ def replace(map, fileInName, fileOutName):
                 line = string.replace(line, old, new)
             with open(fileOutName,'a') as fileout:
                 fileout.write(line)
+    for line in moreLines:
+        for old, new in replace_items:
+            line = string.replace(line, old, new)
+        with open(fileOutName,'a') as fileout:
+            fileout.write(line)
 
 ################################
 ##### Validation functions #####
@@ -94,6 +99,8 @@ def run_validation(dataset,globalTag,run,stream,eventContent,**kwargs):
     The primary validation routine. Creates working directories and submits jobs to crab.
     '''
     force = kwargs.pop('force',False)
+    triggers = kwargs.pop('triggers',[])
+    dryRun = kwargs.pop('dryRun',False)
     runCrab = False
     # create run working directory
     rundir = 'run_%s' % run
@@ -145,27 +152,41 @@ def run_validation(dataset,globalTag,run,stream,eventContent,**kwargs):
     cfgFileName='validation_%s_cfg.py' % run
     crabFileName='crab_%s_cfg.py' % run
     htmlFileName = "Summary.html"
-    macroFileName = "makePlots.C"
-    SingleMuOpen_macroFileName = "SingleMuOpen_makePlots.C"
-    SingleMuBeamHalo_macroFileName = "SingleMuBeamHalo_makePlots.C"
+    macroFileName = {}
+    macroFileName['All'] = "makePlots.C"
+    for trigger in triggers:
+        macroFileName[trigger] = "%s_makePlots.C" % trigger
     procFileName = "secondStep.py"
     outFileName='valHists_run%s_%s.root' % (run, stream)
     Time=time.strftime("%Y/%m/%d %H:%M:%S", time.localtime())
 
     symbol_map_html = { 'RUNNUMBER':run, 'NEVENT':num, "DATASET":dataset, "CMSSWVERSION":Release, "GLOBALTAG":globalTag, "DATE":Time }
-    symbol_map_macro = { 'FILENAME':outFileName, 'TEMPLATEDIR':TEMPLATE_PATH }
-    symbol_map_macro_SingleMuOpen = { 'FILENAME':'SingleMuOpen_'+outFileName, 'TEMPLATEDIR':TEMPLATE_PATH }
-    symbol_map_macro_SingleMuBeamHalo = { 'FILENAME':'SingleMuBeamHalo_'+outFileName, 'TEMPLATEDIR':TEMPLATE_PATH }
+    symbol_map_macro = {}
+    symbol_map_macro['All'] = { 'FILENAME':outFileName, 'TEMPLATEDIR':TEMPLATE_PATH }
+    for trigger in triggers:
+        symbol_map_macro[trigger] = { 'FILENAME':trigger+'_'+outFileName, 'TEMPLATEDIR':TEMPLATE_PATH }
     symbol_map_proc = { 'TEMPLATEDIR':TEMPLATE_PATH, 'OUTPUTFILE':outFileName, 'RUNNUMBER':run, 'NEWDIR':rundir, 'CFGFILE':cfgFileName, 'STREAM':stream }
     symbol_map_cfg = { 'NEVENT':num, 'GLOBALTAG':globalTag, "OUTFILE":outFileName, 'DATASET':dataset, 'RUNNUMBER':run}
     symbol_map_crab = { 'GLOBALTAG':globalTag, "OUTFILE":outFileName, 'DATASET':dataset, 'RUNNUMBER':run, 'STREAM':stream, 'EVENTCONTENT':eventContent}
 
+    trigger_cfg = []
+    trigger_proc = []
+    for trigger in triggers:
+        trigger_cfg += ["process.triggerSelection%s = process.triggerSelection.clone(triggerConditions=cms.vstring('%s'))\n" %(trigger, trigger)]
+        trigger_cfg += ["process.cscValidation%s = process.cscValidation.clone(rootFileName=cms.untracked.string('%s_OUTFILE'))\n" % (trigger, trigger)]
+        trigger_cfg += ["process.p%s = cms.Path(process.gtDigis * process.triggerSelection%s * process.muonCSCDigis * process.csc2DRecHits * process.cscSegments * process.cscValidation%s)\n" % (trigger, trigger, trigger)]
+        trigger_proc += ["print '%s'\n" % trigger]
+        trigger_proc += ['os.system("root -l -q -b %s_makePlots.C")\n' % trigger]
+        trigger_proc += ['os.system("mkdir -p /afs/cern.ch/cms/CAF/CMSCOMM/COMM_CSC/CSCVAL/results/results/runRUNNUMBER/STREAM/Site/PNGS/%s")\n' % trigger ]
+        trigger_proc += ['os.system("mv *.png /afs/cern.ch/cms/CAF/CMSCOMM/COMM_CSC/CSCVAL/results/results/runRUNNUMBER/STREAM/Site/PNGS/%s")\n' % trigger ]
+
+
     replace(symbol_map_html,templateHTMLFilePath, htmlFileName)
-    replace(symbol_map_macro,templateRootMacroPath, macroFileName)
-    replace(symbol_map_macro_SingleMuOpen,templateRootMacroPath, SingleMuOpen_macroFileName)
-    replace(symbol_map_macro_SingleMuBeamHalo,templateRootMacroPath, SingleMuBeamHalo_macroFileName)
-    replace(symbol_map_proc,templateSecondStepPath, procFileName)
-    replace(symbol_map_cfg,templatecfgFilePath, cfgFileName)
+    replace(symbol_map_macro['All'],templateRootMacroPath, macroFileName['All'])
+    for trigger in triggers:
+        replace(symbol_map_macro[trigger],templateRootMacroPath, macroFileName[trigger])
+    replace(symbol_map_proc,templateSecondStepPath, procFileName, trigger_proc)
+    replace(symbol_map_cfg,templatecfgFilePath, cfgFileName, trigger_cfg)
     replace(symbol_map_crab,templatecrabFilePath, crabFileName)
 
     os.system("chmod 755 secondStep.py")
@@ -176,8 +197,12 @@ def run_validation(dataset,globalTag,run,stream,eventContent,**kwargs):
             + '  "runnum" : "%s",\n' % run \
             + '  "events" : "%s",\n' % num \
             + '  "globaltag" : "%s",\n' % globalTag \
-            + '  "rundate" : "%s"\n' % Time \
-            + '}'
+            + '  "rundate" : "%s",\n' % Time \
+            + '  "triggers" : [\n' \
+            + '    "All"'
+    for trigger in triggers:
+        jsonStr += ',\n    "%s"' % trigger
+    jsonStr += '\n  ]\n}'
 
     with open('runParams.json','w') as file:
         file.write(jsonStr)
@@ -237,7 +262,7 @@ def run_validation(dataset,globalTag,run,stream,eventContent,**kwargs):
                 fileListString += "    '%s'" % f
 
             symbol_map_cfg = { 'NEVENT':num, 'GLOBALTAG':globalTag, "OUTFILE":outFileName, 'DATASET':dataset, 'RUNNUMBER':run, 'FILELIST': fileListString, 'VERSION': str(j)}
-            replace(symbol_map_cfg,templatecfgFilePath, cfgFileName)
+            replace(symbol_map_cfg,templatecfgFilePath, cfgFileName, trigger_cfg)
 
             # create a submission script
             sh = open("run_%i.sh" % j, "w")
@@ -251,14 +276,14 @@ def run_validation(dataset,globalTag,run,stream,eventContent,**kwargs):
             sh.write("cd - \n")
             sh.write('cmsRun %s/%s\n' % (rundir, cfgFileName))
             sh.write('cmsStage -f %s /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s\n' % (outFileName, stream, run, eventContent, outFileName))
-            sh.write('cmsStage -f SingleMuOpen_%s /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/SingleMuOpen_%s\n' % (outFileName, stream, run, eventContent, outFileName))
-            sh.write('cmsStage -f SingleMuBeamHalo_%s /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/SingleMuBeamHalo_%s\n' % (outFileName, stream, run, eventContent, outFileName))
+            for trigger in triggers:
+                sh.write('cmsStage -f %s_%s /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s_%s\n' % (trigger, outFileName, stream, run, eventContent, trigger, outFileName))
             sh.write('cmsStage -f TPEHists_%i.root /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/TPEHists_%i.root\n' % (j, stream, run, eventContent, j))
             sh.close()
 
             print "Submitting job %i of %i" % (j+1, numJobs)
             queue = '1nh' if 'Express' in stream else '8nh'
-            subprocess.check_call("bsub -q %s -J %s_%s_%i < run_%i.sh" % (queue, run, stream, j, j), shell=True)
+            if not dryRun: subprocess.check_call("bsub -q %s -J %s_%s_%i < run_%i.sh" % (queue, run, stream, j, j), shell=True)
 
     os.chdir('../')
 
@@ -270,6 +295,8 @@ def process_output(dataset,globalTag,**kwargs):
     '''
     force = kwargs.pop('force',False)
     runN = kwargs.pop('run',0)
+    triggers = kwargs.pop('triggers',[])
+    dryRun = kwargs.pop('dryRun',False)
     [filler, stream, version, eventContent] = dataset.split('/')
     os.chdir(stream)
 
@@ -305,9 +332,10 @@ def process_output(dataset,globalTag,**kwargs):
         os.chdir(rundir)
 
         tpeOut = 'TPEHists.root'
-        valOut = 'valHists_run%s_%s.root' % (run, stream)
-        SingleMuOpen_valOut = 'SingleMuOpen_valHists_run%s_%s.root' % (run, stream)
-        SingleMuBeamHalo_valOut = 'SingleMuBeamHalo_valHists_run%s_%s.root' % (run, stream)
+        valOut = {}
+        valOut['All'] = 'valHists_run%s_%s.root' % (run, stream)
+        for trigger in triggers:
+            valOut[trigger] = '%s_valHists_run%s_%s.root' % (trigger, run, stream)
 
         if runCrab:
             # get last job (in case we reprocessed)
@@ -319,16 +347,17 @@ def process_output(dataset,globalTag,**kwargs):
             jobVersion = 'job'
             fileDir = '%s/%s/%s' % (BATCH_PATH,stream,job)
         tpeFiles = []
-        valFiles = []
-        SingleMuOpen_valFiles = []
-        SingleMuBeamHalo_valFiles = []
+        valFiles = {}
+        valFiles['All'] = []
+        for trigger in triggers:
+            valFiles[trigger] = []
         for file in subprocess.Popen('/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select ls %s' % (fileDir), shell=True,stdout=pipe).communicate()[0].splitlines():
             if file==tpeOut or file==valOut: continue
             if file[0:3]=='TPE': tpeFiles += [file]
-            if file[0:3]=='val': valFiles += [file]
-            if file[0:16]=='SingleMuOpen_val': SingleMuOpen_valFiles += [file]
-            if file[0:20]=='SingleMuBeamHalo_val': SingleMuBeamHalo_valFiles += [file]
-        nFiles = len(valFiles)
+            if file[0:3]=='val': valFiles['All'] += [file]
+            for trigger in triggers:
+                if file.startswith('%s_val' % trigger): valFiles[trigger] += [file]
+        nFiles = len(valFiles['All'])
 
         # see if we need to remerge things
         processedString = '%s_%i' %(jobVersion, nFiles)
@@ -364,29 +393,23 @@ def process_output(dataset,globalTag,**kwargs):
                 tpeMergeString += ' root://eoscms.cern.ch/%s/%s' % (fileDir, tpe)
             sh.write(tpeMergeString+" \n")
             sh.write('cmsStage -f %s /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s\n' % (tpeOut, stream, run, eventContent, tpeOut))
-        if valFiles:
+        if valFiles['All']:
             print "Merging valHists"
-            valMergeString = 'hadd -f %s' % valOut
-            for val in valFiles:
+            valMergeString = 'hadd -f %s' % valOut['All']
+            for val in valFiles['All']:
                 valMergeString += ' root://eoscms.cern.ch/%s/%s' % (fileDir, val)
             sh.write(valMergeString+" \n")
-            sh.write('cmsStage -f %s /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s\n' % (valOut, stream, run, eventContent, valOut))
-        if SingleMuOpen_valFiles:
-            print "Merging SingleMuOpen_valHists"
-            valMergeString = 'hadd -f %s' % SingleMuOpen_valOut
-            for val in SingleMuOpen_valFiles:
-                valMergeString += ' root://eoscms.cern.ch/%s/%s' % (fileDir, val)
-            sh.write(valMergeString+" \n")
-            sh.write('cmsStage -f %s /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s\n' % (SingleMuOpen_valOut, stream, run, eventContent, SingleMuOpen_valOut))
-        if SingleMuBeamHalo_valFiles:
-            print "Merging SingleMuBeamHalo_valHists"
-            valMergeString = 'hadd -f %s' % SingleMuBeamHalo_valOut
-            for val in SingleMuBeamHalo_valFiles:
-                valMergeString += ' root://eoscms.cern.ch/%s/%s' % (fileDir, val)
-            sh.write(valMergeString+" \n")
-            sh.write('cmsStage -f %s /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s\n' % (SingleMuBeamHalo_valOut, stream, run, eventContent, SingleMuBeamHalo_valOut))
+            sh.write('cmsStage -f %s /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s\n' % (valOut['All'], stream, run, eventContent, valOut['All']))
+        for trigger in triggers:
+            if valFiles[trigger]:
+                print "Merging %s_valHists" % trigger
+                valMergeString = 'hadd -f %s' % valOut[trigger]
+                for val in valFiles[trigger]:
+                    valMergeString += ' root://eoscms.cern.ch/%s/%s' % (fileDir, val)
+                sh.write(valMergeString+" \n")
+                sh.write('cmsStage -f %s /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s\n' % (valOut[trigger], stream, run, eventContent, SingleMuOpen[trigger]))
         sh.close()
-        subprocess.check_call("bsub -q 8nh -J %s_%smerge < merge.sh" % (run,stream), shell=True)
+        if not dryRun: subprocess.check_call("bsub -q 8nh -J %s_%smerge < merge.sh" % (run,stream), shell=True)
 
         runsToPlot += [[run,job]]
 
@@ -400,9 +423,10 @@ def process_output(dataset,globalTag,**kwargs):
         os.chdir(rundir)
 
         tpeOut = 'TPEHists.root'
-        valOut = 'valHists_run%s_%s.root' % (run, stream)
-        SingleMuOpen_valOut = 'SingleMuOpen_valHists_run%s_%s.root' % (run, stream)
-        SingleMuBeamHalo_valOut = 'SingleMuBeamHalo_valHists_run%s_%s.root' % (run, stream)
+        valOut = {}
+        valOut['All'] = 'valHists_run%s_%s.root' % (run, stream)
+        for trigger in triggers:
+            valOut[trigger] = '%s_valHists_run%s_%s.root' % (trigger, run, stream)
 
         # wait for job to finish then copy over
         print("Waiting on run %s" % run)
@@ -413,10 +437,10 @@ def process_output(dataset,globalTag,**kwargs):
             print('.')
             print("Run %s merged" % run) 
             subprocess.call('cmsStage -f /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s %s' % (stream, run, eventContent, tpeOut, tpeOut), shell=True)
-            valRet = subprocess.call('cmsStage -f /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s %s' % (stream, run, eventContent, valOut, valOut), shell=True)
-            SingleMuOpen_valRet = subprocess.call('cmsStage -f /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s %s' % (stream, run, eventContent, SingleMuOpen_valOut, SingleMuOpen_valOut), shell=True)
-            SingleMuBeamHalo_valRet = subprocess.call('cmsStage -f /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s %s' % (stream, run, eventContent, SingleMuBeamHalo_valOut, SingleMuBeamHalo_valOut), shell=True)
-            if not valRet: os.system("./secondStep.py")
+            valRet = subprocess.call('cmsStage -f /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s %s' % (stream, run, eventContent, valOut['All'], valOut['All']), shell=True)
+            for trigger in triggers:
+                trigRet = subprocess.call('cmsStage -f /store/group/dpg_csc/comm_csc/cscval/batch_output/%s/run%s_%s/%s %s' % (stream, run, eventContent, valOut[trigger], valOut[trigger]), shell=True)
+            if not valRet and not dryRun: os.system("./secondStep.py")
             subprocess.call('rm *.root', shell=True)
 
         os.chdir('../')
@@ -442,9 +466,10 @@ def process_dataset(dataset,globalTag,**kwargs):
     '''
     Script to call validation of multiple runs
       kwargs:
-        argument    type    default    comment
-        run         int     0          if nonzero, will process a single run, else, process all available
-        force       bool    False      do a run regardless if processed already or number of events
+        argument    type      default    comment
+        run         int       0          if nonzero, will process a single run, else, process all available
+        force       bool      False      do a run regardless if processed already or number of events
+        triggers    list(str) []         list of additional triggers to run over
     '''
     run = kwargs.pop('run',0)
     force = kwargs.pop('force',False)
@@ -476,7 +501,7 @@ def process_dataset(dataset,globalTag,**kwargs):
             print "Processing run %s" % str(run)
             with open(procFile, 'a') as file:
                 file.write(procString+'\n')
-            run_validation(dataset,globalTag,str(run),stream,eventContent,force=force)
+            run_validation(dataset,globalTag,str(run),stream,eventContent,force=force,**kwargs)
     else:
         # query DAS and get list of runs
         newruns = subprocess.Popen("./das_client.py --query='run dataset="+dataset+"' --limit=0", shell=True, stdout=pipe).communicate()[0].splitlines()
@@ -490,7 +515,7 @@ def process_dataset(dataset,globalTag,**kwargs):
                 file.write(procString+'\n')
             if int(num) > 50000: # only care about long runs
                 print "Processing run %s" % rn
-                run_validation(dataset,globalTag,str(rn),stream,eventContent,force=force)
+                run_validation(dataset,globalTag,str(rn),stream,eventContent,force=force,**kwargs)
 
     os.chdir('../')
 
@@ -509,7 +534,9 @@ def parse_command_line(argv):
     parser.add_argument('-ro', '--retrieveOutput', action='store_true',help='Retrieve the output of a previous run and produce the HTML.')
     #parser.add_argument('-rh', '--reprocessHistograms', action='store_true',help='Reprocess histograms (in case of changes to template, rather than CSCValidation.cc).')
     parser.add_argument('-br', '--buildRunlist', action='store_true',help='Build the runlist.json file for the website.')
+    parser.add_argument('-dr', '--dryRun', action='store_true',help='Don\'t submit, just create the objects')
     parser.add_argument('-f','--force', action='store_true', help='Force a recipe (even if already processed).')
+    parser.add_argument('-t','--triggers', nargs='*', help='Optionally run on additional triggers.')
 
     args = parser.parse_args(argv)
     return args
@@ -518,6 +545,8 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
     args = parse_command_line(argv)
+
+    if not args.triggers: args.triggers = []
 
     ds = args.dataset.split('/')
     if len(ds) != 4: 
@@ -529,13 +558,13 @@ def main(argv=None):
         return 0
 
     if args.retrieveOutput:
-        process_output(args.dataset, args.globalTag, force=args.force, run=args.runNumber)
+        process_output(args.dataset, args.globalTag, force=args.force, run=args.runNumber,triggers=args.triggers,dryRun=args.dryRun)
         return 0
 
     #if args.reprocessHistograms:
     #    return 0
 
-    process_dataset(args.dataset, args.globalTag, run=args.runNumber, force=args.force)
+    process_dataset(args.dataset, args.globalTag, run=args.runNumber, force=args.force,triggers=args.triggers,dryRun=args.dryRun)
     
     return 0
 
