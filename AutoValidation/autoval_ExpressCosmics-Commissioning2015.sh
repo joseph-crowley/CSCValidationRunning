7daa45a46dadf49cc7f9031d4dda023057020d9d
Devin Taylor <dntaylor@wisc.edu>
2015-06-03 16:55:48 +0200

    updating for ROOT6

---
+++ AutoValidation/autoval_ExpressCosmics-Commissioning2015.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+aklog
+
+echo "Setup environment"
+
+source /afs/cern.ch/cms/cmsset_default.sh
+
+cd /afs/cern.ch/cms/CAF/CMSCOMM/COMM_CSC/CSCVAL/AUTOVAL/CMSSW_7_4_2/src/CSCValidationRunning/AutoValidation
+
+eval `scramv1 runtime -sh`
+scram b
+
+echo "Initiate validation script"
+
+#cern-get-sso-cookie --krb -r -u https://cmswbm.web.cern.ch/cmswbm/cmsdb/servlet/RunSummary -o ~/private/ssocookie.txt
+./run_cscval.py /ExpressCosmics/Commissioning2015-Express-v1/FEVT GR_E_V47 -t HLT_L1SingleMuOpen_v6 HLT_L1SingleMuBeamHalo_v1
