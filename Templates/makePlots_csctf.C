ff65c7d9a9f8b19c87560a3033f8ac81e014acea
Devin Taylor <dntaylor@wisc.edu>
2015-08-14 17:02:20 +0200

    adding wire and strip occupancies

--- Templates/makePlots_csctf.C
+++ Templates/makePlots_csctf.C
@@ -27,7 +27,47 @@
   Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/CSCTF_Track_ModeVsQual", f1, true, "csctfTrackModeVQual.png", true);
   Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/CSCTF_bx", f1, true, "csctfBx.png", true);
   Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/CSCTF_bx_H", f1, true, "csctfBxHalo.png", true);
-  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/CSCTF_occupancies", f1, true, "csctOccupancies.png", true);
-  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/CSCTF_occupancies_H", f1, true, "csctOccupanciesHalo.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/CSCTF_occupancies", f1, true, "csctfOccupancies.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/CSCTF_occupancies_H", f1, true, "csctfOccupanciesHalo.png", true);
+
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEminus11", f1, true, "csctfStripm11.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEminus12", f1, true, "csctfStripm12.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEminus13", f1, true, "csctfStripm13.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEminus21", f1, true, "csctfStripm21.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEminus22", f1, true, "csctfStripm22.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEminus31", f1, true, "csctfStripm31.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEminus32", f1, true, "csctfStripm32.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEminus41", f1, true, "csctfStripm41.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEminus42", f1, true, "csctfStripm42.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEplus11", f1, true, "csctfStripp11.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEplus12", f1, true, "csctfStripp12.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEplus13", f1, true, "csctfStripp13.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEplus21", f1, true, "csctfStripp21.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEplus22", f1, true, "csctfStripp22.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEplus31", f1, true, "csctfStripp31.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEplus32", f1, true, "csctfStripp32.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEplus41", f1, true, "csctfStripp41.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_strip_MEplus42", f1, true, "csctfStripp42.png", true);
+
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEminus11", f1, true, "csctfWirem11.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEminus12", f1, true, "csctfWirem12.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEminus13", f1, true, "csctfWirem13.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEminus21", f1, true, "csctfWirem21.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEminus22", f1, true, "csctfWirem22.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEminus31", f1, true, "csctfWirem31.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEminus32", f1, true, "csctfWirem32.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEminus41", f1, true, "csctfWirem41.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEminus42", f1, true, "csctfWirem42.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEplus11", f1, true, "csctfWirep11.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEplus12", f1, true, "csctfWirep12.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEplus13", f1, true, "csctfWirep13.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEplus21", f1, true, "csctfWirep21.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEplus22", f1, true, "csctfWirep22.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEplus31", f1, true, "csctfWirep31.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEplus32", f1, true, "csctfWirep32.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEplus41", f1, true, "csctfWirep41.png", true);
+  Draw2DTempPlot("DQMData/Run RUNNUMBER/L1T/Run summary/L1TCSCTF/csc_wire_MEplus42", f1, true, "csctfWirep42.png", true);
+
+
 
 }
