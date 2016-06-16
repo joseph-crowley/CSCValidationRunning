922de9fcaf79ccabc0fd0cad6323a9c5b59723de
Devin Taylor <dntaylor@wisc.edu>
2016-06-16 16:29:25 +0200

    add era, trigger emulator still not correct, keep off

--- AutoValidation/run_cscval.py
+++ AutoValidation/run_cscval.py
@@ -24,7 +24,7 @@ import errno
 import math
 from das_client import get_data, DASOptionParser
 
-MINRUN = 272728 # Run2016B
+MINRUN = 274516 # start TS1
 
 pipe = subprocess.PIPE
 Release = subprocess.Popen('echo $CMSSW_VERSION', shell=True, stdout=pipe).communicate()[0]
