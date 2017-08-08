7d935464b1e2d238736bc8827122b355ef32145f
wsicheng <sicheng@physics.ucsb.edu>
2017-08-09 00:27:30 +0200

    Add simple script for signaling auto remerging

---
+++ AutoValidation/remerge.sh
@@ -0,0 +1,23 @@
+runDir=${1:-/eos/cms/store/group/dpg_csc/comm_csc/cscval/www/results}
+batchDir=${2:-/eos/cms/store/group/dpg_csc/comm_csc/cscval/batch_output}
+
+declare -a datasets=(ExpressPhysics DoubleMuon SingleMuon Cosmics)
+for ds in ${datasets[@]}; do
+    for i in $(ls "$ds"); do
+        [[ ! ${i:0:3} == "run" ]] && continue
+        j="run${i:4:6}"
+        testfile="$runDir/$j/$ds/Site/PNGS/Efficiency_hSEff.png"
+        if [ ! -f $testfile ]; then
+            # echo rm -r $ds/$i
+            # echo rm -r $batchDir/$ds/${j}_RAW
+            # rm -r $ds/$i
+            # rm -r $batchDir/$ds/${j}_RAW
+            [ -f $ds/$i/out.txt ] || continue
+            njob=`cat $ds/$i/out.txt`
+            [[ $njob == "job_0" ]] || [[ $njob == "" ]] && continue
+            echo $ds/$i/out.txt
+            # cat $ds/$i/out.txt
+            echo "job_0" > $ds/$i/out.txt
+        fi
+    done
+done            
