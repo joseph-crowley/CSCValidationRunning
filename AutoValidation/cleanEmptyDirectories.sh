74af98cd7f3196deafe0e4bd7af4f98e29fa3d9e
Andrew Wisecarver <andrew.lloyd.wisecarver@cern.ch>
2017-09-08 15:34:11 +0200

    updated cleanEmptyDirectories.sh

--- AutoValidation/cleanEmptyDirectories.sh
+++ AutoValidation/cleanEmptyDirectories.sh
@@ -1,4 +1,7 @@
-BASEDIR=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_CSC/CSCVAL/results/results
+#!/bin/bash
+
+BASEDIR=/eos/cms/store/group/dpg_csc/comm_csc/cscval/www/results
+echo "removing directories in EOS www folder w/ no CSC information (no hORecHitsSerial.png)"
 ls -l $BASEDIR/*/*/Site/PNGS/hORecHitsSerial.png |  sed -n -E "s/.*(run[0-9]*\/[a-zA-Z]*[^\/])\/.*/\1/p" | sort > hasCSC.txt
 ls -l $BASEDIR/*/*/ |  sed -n -E "s/.*(run[0-9]*\/[a-zA-Z]*[^\/])\/.*/\1/p" | sort > allDirs.txt
 comm -3 allDirs.txt hasCSC.txt > toRemove.txt
