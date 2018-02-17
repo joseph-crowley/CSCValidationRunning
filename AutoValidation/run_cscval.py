0bdcfc8eddcea4a8b3b8ed54c41d78f8030b43f6
Andrew Wisecarver <andrew.lloyd.wisecarver@cern.ch>
2018-02-17 17:27:10 +0100

    new EMTF updates for CMSSW_10_0_0_pre3

--- AutoValidation/run_cscval.py
+++ AutoValidation/run_cscval.py
@@ -24,7 +24,7 @@ import errno
 import math
 from dbs.apis.dbsClient import DbsApi
 
-MINRUN = 303400 # test
+MINRUN = 302029 # test
 
 pipe = subprocess.PIPE
 Release = subprocess.Popen('echo $CMSSW_VERSION', shell=True, stdout=pipe).communicate()[0]
